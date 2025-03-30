e2cdc151d42fbd58dc37498a8aba5bb2246cf11a
juan <juanekipes@gmail.com>
2025-03-30 19:53:28 -0300

    initial commit

---
+++ src/test/java/ar/com/juanek/testinggit/TestingGitApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.juanek.testinggit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
